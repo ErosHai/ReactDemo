a86db4c40dfc83bddd88234bbb3080010d4fa767
ErosHai <zhl826882143@gmail.com>
2022-03-16 21:39:28 +0800

    First Push

---
+++ src/pages/Home/index.js
@@ -0,0 +1,27 @@
+import Edit from './components/Edit'
+import List from './components/List'
+import { useState } from 'react'
+import styled from 'styled-components'
+
+const Container = styled.h1`
+	width: 500px;
+	height: auto;
+	padding: 50px 10px;
+	margin: 0 auto;
+	border: 2px solid palevioletred;
+	font-size: 30px;
+	text-align: center;
+	color: palevioletred;
+`
+const Home = () => {
+	const [data, setData] = useState([])
+
+	return (
+		<Container>
+			<Edit setData={setData} data={data} />
+			<List listData={data} />
+		</Container>
+	)
+}
+
+export default Home
